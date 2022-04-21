c3d8609703c370ace02639be32f6e81f8bc42b78
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-04-21 09:57:16 +0200

    Implemented Translator.to_language_or_skip_test

--- miasm/ir/translators/translator.py
+++ miasm/ir/translators/translator.py
@@ -33,6 +33,18 @@ class Translator(object):
         raise NotImplementedError("Unknown target language: %s" % target_lang)
 
     @classmethod
+    def to_language_or_skip_test(cls, target_lang, *args, **kwargs):
+        """Convenience methods for tests
+        Same behavior as 'to_language', except that it skips the current test if missing instead of raising a NotImplementedError.
+        """
+        import pytest
+
+        try:
+            return cls.to_language(target_lang, *args, **kwargs)
+        except NotImplementedError as error:
+            pytest.skip(error)
+
+    @classmethod
     def available_languages(cls):
         "Return the list of registered languages"
         return [translator.__LANG__ for translator in cls.available_translators]
@@ -124,4 +136,3 @@ class Translator(object):
                 self._cache[expr] = ret
                 return ret
         raise ValueError("Unhandled type for %s" % expr)
-
